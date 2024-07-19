9bd3d2af11f50a241e3b52f049c4fd59ed913f5c
Yasin Jafari <yasinj7658@gmail.com>
2024-07-20 00:20:28 +0330

    Update BrandController

--- src/controllers/BrandController.ts
+++ src/controllers/BrandController.ts
@@ -4,13 +4,30 @@ import { BrandService } from "../services/BrandService";
 const brandService = new BrandService();
 
 export class BrandController {
-  async create(req: Request, res: Response) {}
+  async create(req: Request, res: Response) {
+    const brand = await brandService.create(req.body);
+    res.json(brand);
+  }
 
-  async update(req: Request, res: Response) {}
+  async update(req: Request, res: Response) {
+    const { id } = req.params;
+    const brand = await brandService.update(Number(id), req.body);
+  }
 
-  async findAll(req: Request, res: Response) {}
+  async findAll(req: Request, res: Response) {
+    const brands = await brandService.findAll();
+    res.json(brands);
+  }
 
-  async findOne(req: Request, res: Response) {}
+  async findOne(req: Request, res: Response) {
+    const { id } = req.params;
+    const brand = await brandService.findOne(Number(id));
+    res.json(brand);
+  }
 
-  async delete(req: Request, res: Response) {}
+  async delete(req: Request, res: Response) {
+    const { id } = req.params;
+    await brandService.delete(Number(id));
+    res.sendStatus(204);
+  }
 }
