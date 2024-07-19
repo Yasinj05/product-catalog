30c2d6f9e65e86a1e3de320593ee9c4a775bfbfe
Yasin Jafari <yasinj7658@gmail.com>
2024-07-19 19:28:13 +0330

    Update BrandService

--- src/services/BrandService.ts
+++ src/services/BrandService.ts
@@ -1,4 +1,27 @@
 import { Brand } from "../entities/Brand";
 import { AppDataSource } from "../utils/db";
 
-export class BrandService {}
+export class BrandService {
+  private brandRepository = AppDataSource.getRepository(Brand);
+
+  async create(brand: Brand) {
+    return this.brandRepository.save(brand);
+  }
+
+  async update(id: number, brand: Partial<Brand>) {
+    await this.brandRepository.update(id, brand);
+    return this.brandRepository.findOneBy({ id });
+  }
+
+  async findAll() {
+    return this.brandRepository.find();
+  }
+
+  async findOne(id: number) {
+    return this.brandRepository.findOneBy({ id });
+  }
+
+  async delete(id: number) {
+    return this.brandRepository.findOneBy({ id });
+  }
+}
