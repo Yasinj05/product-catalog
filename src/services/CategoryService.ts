b2bc6e40dc42b724f17874af2ba97bd26a3b1184
Yasin Jafari <yasinj7658@gmail.com>
2024-07-19 19:40:05 +0330

    Update CategoryService

--- src/services/CategoryService.ts
+++ src/services/CategoryService.ts
@@ -1,4 +1,27 @@
 import { Category } from "../entities/Category";
 import { AppDataSource } from "../utils/db";
 
-export class CategoryService {}
+export class CategoryService {
+  private categoryRepository = AppDataSource.getRepository(Category);
+
+  async create(id: number, category: Category) {
+    return this.categoryRepository.save(category);
+  }
+
+  async update(id: number, category: Partial<Category>) {
+    await this.categoryRepository.update(id, category);
+    return this.categoryRepository.findOneBy({ id });
+  }
+
+  async findAll() {
+    return this.categoryRepository.find();
+  }
+
+  async findOne(id: number) {
+    return this.categoryRepository.findOneBy({ id });
+  }
+
+  async delete(id: number) {
+    return this.categoryRepository.findOneBy({ id });
+  }
+}
