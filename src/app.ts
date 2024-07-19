f0e1854d2dbeef46af174b56edff68f8e0a71ce2
Yasin Jafari <yasinj7658@gmail.com>
2024-07-20 01:05:03 +0330

    Update the app.ts for CategoryRoutes

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,12 @@
 import "reflect-metadata";
 import express from "express";
 import brandRoutes from "./routes/BrandRoutes";
+import categoryRoutes from "./routes/CategoryRoutes";
 
 const app = express();
 
 app.use(express.json());
 app.use("/api", brandRoutes);
+app.use("/api", categoryRoutes);
 
 export default app;
