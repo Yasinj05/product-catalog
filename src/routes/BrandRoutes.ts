bf3ce6a88f74ff7f3a832c9780412f1af80dc486
Yasin Jafari <yasinj7658@gmail.com>
2024-07-20 01:02:27 +0330

    Update BrandRoutes

--- src/routes/BrandRoutes.ts
+++ src/routes/BrandRoutes.ts
@@ -4,10 +4,10 @@ import { BrandController } from "../controllers/BrandController";
 const router = Router();
 const brandController = new BrandController();
 
-router.post("/brands", brandController.create);
-router.put("/brands/:id", brandController.update);
-router.get("/brands", brandController.findAll);
-router.get("/brands/:id", brandController.findOne);
-router.delete("/brands/:id", brandController.delete);
+router.post("/brands", brandController.create.bind(brandController));
+router.put("/brands/:id", brandController.update.bind(brandController));
+router.get("/brands", brandController.findAll.bind(brandController));
+router.get("/brands/:id", brandController.findOne.bind(brandController));
+router.delete("/brands/:id", brandController.delete.bind(brandController));
 
 export default router;
