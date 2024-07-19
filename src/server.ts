69d39e8c7d8f4e8561925bbd869f9ea1d196317c
Yasin Jafari <yasinj7658@gmail.com>
2024-07-20 00:32:53 +0330

    Update server.ts

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,14 @@
-import app from "./app";
+import "reflect-metadata";
+import { AppDataSource } from "./utils/db";
 import dotenv from "dotenv";
+import app from "./app";
 dotenv.config();
 
-const PORT = process.env.PORT || 3000;
-
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+AppDataSource.initialize()
+  .then(() => {
+    const PORT = process.env.PORT || 3000;
+    app.listen(PORT, () => {
+      console.log(`Server is running on port ${PORT}`);
+    });
+  })
+  .catch((error) => console.log(error));
