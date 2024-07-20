00908448ee10622f1edfd3457b52965dae23b9b6
Yasin Jafari <yasinj7658@gmail.com>
2024-07-20 22:31:50 +0330

    Update Brand and Category routes for validation middlware

--- src/routes/BrandRoutes.ts
+++ src/routes/BrandRoutes.ts
@@ -1,11 +1,23 @@
 import { Router } from "express";
 import { BrandController } from "../controllers/BrandController";
+import {
+  validateCreateBrand,
+  validateUpdateBrand,
+} from "../validations/brandValidation";
 
 const router = Router();
 const brandController = new BrandController();
 
-router.post("/brands", brandController.create.bind(brandController));
-router.put("/brands/:id", brandController.update.bind(brandController));
+router.post(
+  "/brands",
+  validateCreateBrand,
+  brandController.create.bind(brandController)
+);
+router.put(
+  "/brands/:id",
+  validateUpdateBrand,
+  brandController.update.bind(brandController)
+);
 router.get("/brands", brandController.findAll.bind(brandController));
 router.get("/brands/:id", brandController.findOne.bind(brandController));
 router.delete("/brands/:id", brandController.delete.bind(brandController));
--- src/routes/CategoryRoutes.ts
+++ src/routes/CategoryRoutes.ts
@@ -1,12 +1,21 @@
 import { Router } from "express";
 import { CategoryController } from "../controllers/CategoryController";
+import {
+  validateCreateCategory,
+  validateUpdateCategory,
+} from "../validations/categoryValidation";
 
 const router = Router();
 const categoryController = new CategoryController();
 
-router.post("/categories", categoryController.create.bind(categoryController));
+router.post(
+  "/categories",
+  validateCreateCategory,
+  categoryController.create.bind(categoryController)
+);
 router.put(
   "/categories/:id",
+  validateUpdateCategory,
   categoryController.update.bind(categoryController)
 );
 router.get("/categories", categoryController.findAll.bind(categoryController));
