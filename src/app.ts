7036490f5a85c6e25fd31cd2c0e48b5eda7fca06
Yasin Jafari <yasinj7658@gmail.com>
2024-07-20 19:46:13 +0330

    Update app.ts for ItemRoutes

--- src/app.ts
+++ src/app.ts
@@ -2,11 +2,13 @@ import "reflect-metadata";
 import express from "express";
 import brandRoutes from "./routes/BrandRoutes";
 import categoryRoutes from "./routes/CategoryRoutes";
+import itemRoutes from "./routes/ItemRoutes";
 
 const app = express();
 
 app.use(express.json());
 app.use("/api", brandRoutes);
 app.use("/api", categoryRoutes);
+app.use("/api", itemRoutes);
 
 export default app;
