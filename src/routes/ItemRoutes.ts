238532aba545ef0f5ba9a6a99a2c072d38d4dbb7
Yasin Jafari <yasinj7658@gmail.com>
2024-07-24 19:10:16 +0330

    Update the ItemRoutes for validation

--- src/routes/ItemRoutes.ts
+++ src/routes/ItemRoutes.ts
@@ -1,17 +1,29 @@
 import { Router } from "express";
 import { ItemController } from "../controllers/ItemController";
+import {
+  validateCreateItem,
+  validateUpdateItem,
+} from "../middlwares/validations/itemValidation";
 
 const router = Router();
 const itemController = new ItemController();
 
-router.post("/items", itemController.create.bind(itemController));
-router.put("/items/:id", itemController.update.bind(itemController));
+router.post(
+  "/items",
+  validateCreateItem,
+  itemController.create.bind(itemController)
+);
+router.put(
+  "/items/:id",
+  validateUpdateItem,
+  itemController.update.bind(itemController)
+);
 router.get("/items", itemController.findAll.bind(itemController));
 router.get("/items/:id", itemController.findOne.bind(itemController));
 router.delete("/items/:id", itemController.delete.bind(itemController));
 router.patch(
   "/items/:id/MaxStockThreshold",
-  itemController.updateMaxStockThreshold.bind(itemController)
+  itemController.updateStockQuantity.bind(itemController)
 );
 
 export default router;
