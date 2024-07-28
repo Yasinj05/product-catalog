882c21f45cbf23b79ad4881e1f4a0b2c6de4018b
Yasin Jafari <yasinj7658@gmail.com>
2024-07-28 19:34:00 +0330

    Update the app.ts for swagger

--- src/app.ts
+++ src/app.ts
@@ -4,14 +4,20 @@ import brandRoutes from "./routes/BrandRoutes";
 import categoryRoutes from "./routes/CategoryRoutes";
 import itemRoutes from "./routes/ItemRoutes";
 import { errorHandler } from "./middlewares/errorHandler";
+import { setupSwagger } from "./utils/swaggerConfig";
 
 const app = express();
 
+// Middleware
 app.use(express.json());
+app.use(errorHandler);
+
+// Routes
 app.use("/api", brandRoutes);
 app.use("/api", categoryRoutes);
 app.use("/api", itemRoutes);
 
-app.use(errorHandler);
+// Swagger setup
+setupSwagger(app);
 
 export default app;
